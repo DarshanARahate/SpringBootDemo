2dfbfaa4ab4e74f37ef38b4225ef24fe9531340b
darshanrahate <rahate.darshan@gmail.com>
2024-09-06 00:02:20 +0530

    actuator-demo implemented

--- src/main/java/com/example/springbootdemo/MyFirstAPI.java
+++ src/main/java/com/example/springbootdemo/MyFirstAPI.java
@@ -11,4 +11,18 @@ public class MyFirstAPI {
         return "This is my first api";
     }
 
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
 }
