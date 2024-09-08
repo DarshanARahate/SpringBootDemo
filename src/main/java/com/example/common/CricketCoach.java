d65ca1cd43c3f8b6a2d11ac8ea3971c381a8c314
darshanrahate <rahate.darshan@gmail.com>
2024-09-08 22:21:39 +0530

    component-scanning implemented.

--- src/main/java/com/example/common/CricketCoach.java
+++ src/main/java/com/example/common/CricketCoach.java
@@ -1,4 +1,4 @@
-package com.example.springbootdemo;
+package com.example.common;
 
 import org.springframework.stereotype.Component;
 
--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -3,11 +3,14 @@ package com.example.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.example.springbootdemo",
+                "com.example.common"}
+)
 public class SpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
 
 }
