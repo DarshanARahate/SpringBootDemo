446f3145fe2f15fcfb0bd9db9f216abae085b211
darshanrahate <rahate.darshan@gmail.com>
2024-09-22 17:06:08 +0530

    Student Find By Id.

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -10,28 +10,42 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-		return runner -> {
-			createStudent(studentDAO);
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+//			createStudent(studentDAO);
 
-	private void createStudent(StudentDAO studentDAO) {
-		Student student1 = new Student("Ram", "Rama", "ram@gmail.com");
-		Student student2 = new Student("Krushna", "Lala", "krushna@gmail.com");
-		Student student3 = new Student("Mahadev", "Shiv", "mahadev@gmail.com");
-		Student student4 = new Student("Durga", "Devi", "durga@gmail.com");
+            readStudent(studentDAO);
+        };
+    }
 
-		studentDAO.save(student1);
-		studentDAO.save(student2);
-		studentDAO.save(student3);
-		studentDAO.save(student4);
+    private void readStudent(StudentDAO studentDAO) {
+        Student student1 = new Student("Ram", "Rama", "ram@gmail.com");
+        studentDAO.save(student1);
+        Student student2 = new Student("Ram 2", "Rama 2", "ram2@gmail.com");
+        studentDAO.save(student2);
 
-	}
+        int theID = student2.getId();
+        System.out.println("getStudent theID: " + theID);
+        Student getStudent = studentDAO.findById(theID);
+        System.out.println("getStudent : " + getStudent);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        Student student1 = new Student("Ram", "Rama", "ram@gmail.com");
+        Student student2 = new Student("Krushna", "Lala", "krushna@gmail.com");
+        Student student3 = new Student("Mahadev", "Shiv", "mahadev@gmail.com");
+        Student student4 = new Student("Durga", "Devi", "durga@gmail.com");
+
+        studentDAO.save(student1);
+        studentDAO.save(student2);
+        studentDAO.save(student3);
+        studentDAO.save(student4);
+
+    }
 
 }
--- src/main/java/com/example/springbootdemo/dao/StudentDAO.java
+++ src/main/java/com/example/springbootdemo/dao/StudentDAO.java
@@ -4,4 +4,6 @@ import com.example.springbootdemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student student);
+
+    Student findById(Integer id);
 }
